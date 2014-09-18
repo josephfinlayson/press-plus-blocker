3343a01ff95fb4955bbd2997560d3252d2dd4b1f
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-09-18 14:43:53 +0100

    removing setinterval for pages that don't have PP

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,12 +1,16 @@
 'use strict';
-
+var counter = 0
 function blockPressPlus() {
+    counter++
     var popup = document.getElementById('ppUI');
     var overlay = document.getElementById('pressplusOverlay')
     if (popup && overlay) {
     	console.log("blocking PressPlus");
         popup.parentNode.removeChild(popup);
         overlay.parentNode.removeChild(overlay);
+    }
+    if (counter == 60) {
+    	console.log("Probably no PressPlus on this page (waited "+counter/10+" seconds)");
         clearInterval(window.blockPPint);
     }
 }
